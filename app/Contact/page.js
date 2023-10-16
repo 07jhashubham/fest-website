24f9a9b08e15fdc12c9e598caf434ef7bdec5601
07jhashubham <07jha30@gmail.com>
2023-10-17 03:07:50 +0530

    ram ram

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -1,68 +1,68 @@
-"use client"
-import React, { useEffect, useRef } from "react";
-import * as THREE from "three";
+// "use client"
+// import React, { useEffect, useRef } from "react";
+// import * as THREE from "three";
 
-const Page = () => {
-  const scene = new THREE.Scene();
+// const Page = () => {
+//   const scene = new THREE.Scene();
 
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    1,
-    0.1,
-    1000
-  );
-  camera.position.z = 1;
+//   const camera = new THREE.PerspectiveCamera(
+//     75,
+//     1,
+//     0.1,
+//     1000
+//   );
+//   camera.position.z = 1;
 
-  const renderer = new THREE.WebGLRenderer();
+//   const renderer = new THREE.WebGLRenderer();
 
-  const containerRef = useRef();
+//   const containerRef = useRef();
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // This code will only run on the client side.
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      containerRef.current.appendChild(renderer.domElement);
+//   useEffect(() => {
+//     if (typeof window !== "undefined") {
+//       // This code will only run on the client side.
+//       renderer.setSize(window.innerWidth, window.innerHeight);
+//       containerRef.current.appendChild(renderer.domElement);
 
-      const geometry = new THREE.BoxGeometry();
-      const material = new THREE.MeshBasicMaterial({
-        color: 0xffd700,
-        wireframe: true,
-      });
+//       const geometry = new THREE.BoxGeometry();
+//       const material = new THREE.MeshBasicMaterial({
+//         color: 0xffd700,
+//         wireframe: true,
+//       });
 
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
+//       const cube = new THREE.Mesh(geometry, material);
+//       scene.add(cube);
 
-      window.addEventListener("resize", onWindowResize, false);
+//       window.addEventListener("resize", onWindowResize, false);
 
-      function onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        render();
-      }
+//       function onWindowResize() {
+//         camera.aspect = window.innerWidth / window.innerHeight;
+//         camera.updateProjectionMatrix();
+//         renderer.setSize(window.innerWidth, window.innerHeight);
+//         render();
+//       }
 
-      function animate() {
-        requestAnimationFrame(animate);
+//       function animate() {
+//         requestAnimationFrame(animate);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+//         cube.rotation.x += 0.01;
+//         cube.rotation.y += 0.01;
 
-        render();
-      }
+//         render();
+//       }
 
-      function render() {
-        renderer.render(scene, camera);
-      }
+//       function render() {
+//         renderer.render(scene, camera);
+//       }
 
-      animate();
-    }
-  }, []);
+//       animate();
+//     }
+//   }, []);
 
-  return (
-    <div className="h-screen">
-      <div ref={containerRef} className="background"></div>
-    </div>
-  );
-};
+//   return (
+//     <div className="h-screen">
+//       <div ref={containerRef} className="background"></div>
+//     </div>
+//   );
+// };
 
-export default Page;
+// export default Page;
